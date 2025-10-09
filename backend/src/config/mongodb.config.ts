4ee41e2f0c67878bb7696cc08520356586053e8f
Arunpandian C <arunpandiancse25@gmail.com>
2025-10-09 20:41:46 +0530

    feat: add configuration files for database, JWT, and MongoDB

---
+++ backend/src/config/mongodb.config.ts
@@ -0,0 +1,6 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('mongodb', () => ({
+  uri: process.env.MONGODB_URI || 'mongodb://admin:admin@localhost:27017/jezcabs?authSource=admin',
+}));
+
