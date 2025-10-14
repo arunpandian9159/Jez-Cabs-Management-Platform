38db8a5c4810ccebe7a0c45849223ebe50369f65
Arunpandian C <arunpandiancse25@gmail.com>
2025-10-14 21:18:12 +0530

    updates

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -5,7 +5,6 @@ import { X } from "lucide-react"
 import { cn } from "../../lib/utils"
 
 const Dialog = DialogPrimitive.Root
-
 const DialogTrigger = DialogPrimitive.Trigger
 
 const DialogPortal = DialogPrimitive.Portal
