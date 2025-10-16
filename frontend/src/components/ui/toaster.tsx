d377adf46653028cdf115f24fda40e5c695fe752
Arunpandian C <arunpandiancse25@gmail.com>
2025-10-16 21:08:53 +0530

    updates

--- frontend/src/components/ui/toaster.tsx
+++ frontend/src/components/ui/toaster.tsx
@@ -19,7 +19,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
       }}
-      {...props}
+      {...props} 
     />
   )
 }
